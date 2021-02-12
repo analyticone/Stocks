98dd3721af112c6636b3e61e7c6988b5e20aa555
analyticone <steven.kreutzer@yahoo.com>
2021-02-11 21:17:59 -0500

    First release

---
+++ app/src/main/java/com/example/stocktracker/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.stocktracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.codepath.asynchttpclient.callback.TextHttpResponseHandler;
+import com.example.stocktracker.models.Stock;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import okhttp3.Headers;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String API_KEY="33cdc7309b0460999d3a877ee475b2d8";
+    public String stocks = "AAPL,FB,GOOG";
+
+    List<Stock> stockQuotes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        stockQuotes = new ArrayList<>();
+
+        // https://github.com/codepath/CPAsyncHttpClient
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        String url = "https://financialmodelingprep.com/api/v3/quote/" + stocks +  "?apikey=" + API_KEY;
+        client.get(url, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+
+
+                Log.i("MainActivity", json.toString());
+                try {
+                    stockQuotes.addAll(Stock.fromJSonArray(json.jsonArray));
+                    Log.d("MainActivity", "Stocks: " + stockQuotes.size());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+            }
+    });
+
+    }
+}
---
+++ app/src/main/java/com/example/stocktracker/models/Stock.java
@@ -0,0 +1,44 @@
+package com.example.stocktracker.models;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stock {
+    String symbol;
+    String name;
+    double price;
+    double change;
+
+    public Stock(JSONObject jsonObject) throws JSONException {
+        symbol = jsonObject.getString("symbol");
+        name = jsonObject.getString("name");
+        price = jsonObject.getDouble("price");
+        change = jsonObject.getDouble("change");
+        Log.d("Stock", toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Stock{" +
+                "symbol='" + symbol + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", change=" + change +
+                '}';
+    }
+
+    public static List<Stock> fromJSonArray(JSONArray jsonArray) throws JSONException {
+        List<Stock> stocks = new ArrayList<>();
+
+        for (int i=0; i<jsonArray.length(); i++)
+            stocks.add(new Stock(jsonArray.getJSONObject(i)));
+
+        return stocks;
+    }
+}
